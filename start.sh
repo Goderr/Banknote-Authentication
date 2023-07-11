14e51d8ee2832205ba8660a30165e4dfb89131ad
Goderr <115152190+Goderr@users.noreply.github.com>
2023-07-11 16:44:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,1 @@
-start -eu
-
-export PYTHONUNBUFFERED=true
-
-VIRTUALENV = .data/venv
-
-if [! -d $VIRTUALENV ]; then
- python3 -m venv $VIRTUALENV
-fi
-
-if [! -f $VIRTUALENV/bin/pip ]; then
- curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
-fi
-
-$VIRTUALENV/bin/pip install -r requirements.txt
-
-$VIRTUALENV/bin/python3 api.py
+gunicorn main:app -w 1 --log-file -
